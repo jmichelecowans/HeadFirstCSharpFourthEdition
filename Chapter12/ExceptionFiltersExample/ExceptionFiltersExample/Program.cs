86b519910b891e9dd10f622928c06833950650f7
Leandro Humberto Melo dos Santos <lehumberto@msn.com>
2022-08-15 21:59:59 -0300

    Chapter12

---
+++ Chapter12/ExceptionFiltersExample/ExceptionFiltersExample/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace ExceptionFiltersExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstLine = "No first line was read";
+            try
+            {
+                var lines = File.ReadAllLines(args[0]);
+                firstLine = (lines.Length > 0) ? lines[0] : "The file was empty";
+            }
+            catch (IndexOutOfRangeException) when (firstLine != null)
+            {
+                Console.Error.WriteLine("Please specify a filename.");
+            }
+            catch (FileNotFoundException) when (firstLine != null)
+            {
+                Console.Error.WriteLine("Unable to find file: {0}", args[0]);
+            }
+            catch (UnauthorizedAccessException ex) when (firstLine != null)
+            {
+                Console.Error.WriteLine("File {0} could not be accessed: {1}"
+                    , args[0], ex.Message);
+            }
+            catch (IOException ex) when (firstLine != null)
+            {
+                Console.Error.WriteLine("I/O Error while reading the file: {0}", ex.Message);
+            }
+            catch (Exception ex) when (firstLine != null)
+            {
+                Console.Error.WriteLine("Error while reading the file: {0}", ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine(firstLine);
+            }
+        }
+    }
+}
---
+++ Chapter12/FinallyBlockExample/FinallyBlockExample/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace FinallyBlockExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstLine = "No first line was read";
+            try
+            {
+                var lines = File.ReadAllLines(args[0]);
+                firstLine = (lines.Length > 0) ? lines[0] : "The file was empty";
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.Error.WriteLine("Please specify a filename.");
+            }
+            catch (FileNotFoundException)
+            {
+                Console.Error.WriteLine("Unable to find file: {0}", args[0]);
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                Console.Error.WriteLine("File {0} could not be accessed: {1}",
+                args[0], ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine(firstLine);
+            }
+        }
+    }
+}
